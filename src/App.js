c20091c831938828b9b22acee1e6d4abd0d8b809
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2024-01-23 00:16:11 -0500

    files setup and router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="bg-red-600 flex justify-center h-24 m-4 p-5 rounded-2xl">
-    <h1 className="text-5xl font-bold text-white ">
+    <h1 className="text-3xl font-bold text-white ">
     New Peoples App Cpy
   </h1>
   </div>
