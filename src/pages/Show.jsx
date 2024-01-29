f967fc0d47d3c7f30e00441b88155dcab7716495
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2024-01-29 00:15:04 -0500

    show page updated

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -5,9 +5,9 @@ const Show = () => {
   const people= useLoaderData()
   return (
     <div className="flex flex-column flex-wrap justify-center items-center bg-green-900 rounded-lg  m-36 p-12   ">
-      <h1 className='text-white text-3xl'>{people.name}</h1>
+      <h1 className='text-white text-3xl'>Name : {people.name}</h1>
       <img className="rounded-lg m-4 " src={people.image} alt={people.name} />
-      <h3 className='text-white text-3xl'>{people.title}</h3>
+      <h3 className='text-white text-3xl'>Title : {people.title}</h3>
       </div>
   )
 }
